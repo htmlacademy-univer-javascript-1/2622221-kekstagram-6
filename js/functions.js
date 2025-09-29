fe5955b8f26d7958de817dfe7eabc8e63e26cfcb
UstyantsewaME <maria.ustyantsewa@yandex.ru>
2025-09-29 11:50:32 +0500

    Создание функции подсчёта времени

--- js/functions.js
+++ js/functions.js
@@ -13,3 +13,21 @@ const checkPalindrome = function (line) {
   if (finalLine===newLine) {return true;}
   return false;
 };
+const checkTime = function (timeStart, timeEnd, meetStart, meetTime) {
+  const timeToMinutes = function(time) {
+    const parts = time.split(':');
+    const hours = parseInt(parts[0], 10);
+    const minutes = parseInt(parts[1] || 0, 10);
+    return hours * 60 + minutes;
+  };
+  const dayStart = timeToMinutes(timeStart);
+  const dayEnd = timeToMinutes(timeEnd);
+  const meetingStart = timeToMinutes(meetStart);
+  const meetingDuration = meetTime;
+  const meetingEnd = meetingStart + meetingDuration;
+
+  if (meetingStart >= dayStart && meetingEnd <= dayEnd) {
+    return true;
+  }
+  return false;
+};
