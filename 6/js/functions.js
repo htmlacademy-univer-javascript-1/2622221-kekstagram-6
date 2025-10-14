a507d09db6bcf8d71b9ff6dd62f47c552b714c1c
Keks <keks@htmlacademy.ru>
2025-10-14 23:22:51 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/functions.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line no-unused-vars
+const checkString = function  (line, lineLenght) {
+  if (line.length()<=lineLenght) {return true;}
+  return false;
+};
+// eslint-disable-next-line no-unused-vars
+const checkPalindrome = function (line) {
+  const newLine = line.toLowerCase().replaceAll();
+  let finalLine;
+  for (let i = newLine.length(); i > 0; i--) {
+    finalLine+=newLine[i];
+  }
+  if (finalLine===newLine) {return true;}
+  return false;
+};
+const checkTime = function (timeStart, timeEnd, meetStart, meetTime) {
+  const timeToMinutes = function(time) {
+    const parts = time.split(':');
+    const hours = parseInt(parts[0], 10);
+    const minutes = parseInt(parts[1] || 0, 10);
+    return hours * 60 + minutes;
+  };
+  const dayStart = timeToMinutes(timeStart);
+  const dayEnd = timeToMinutes(timeEnd);
+  const meetingStart = timeToMinutes(meetStart);
+  const meetingDuration = meetTime;
+  const meetingEnd = meetingStart + meetingDuration;
+
+  if (meetingStart >= dayStart && meetingEnd <= dayEnd) {
+    return true;
+  }
+  return false;
+};
---
+++ 6/js/main.js
@@ -0,0 +1,8 @@
+import './util.js';
+import './data.js';
+import {renderThumbnails} from './thumbnail.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderThumbnails();
+});
+
