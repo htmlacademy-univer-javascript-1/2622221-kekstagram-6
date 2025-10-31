8e5fdbf3d24ebc82a536cf7cfe82505883deb932
UstyantsewaME <maria.ustyantsewa@yandex.ru>
2025-10-31 15:16:14 +0500

    открывается и закрывается: версия 1

--- js/util.js
+++ js/util.js
@@ -3,6 +3,4 @@ const getRandomElement = (array) => array[Math.floor(Math.random() * array.lengt
 
 const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-// Сделать функции глобальными
-window.getRandomElement = getRandomElement;
-window.getRandomInteger = getRandomInteger;
+export { getRandomElement, getRandomInteger };
